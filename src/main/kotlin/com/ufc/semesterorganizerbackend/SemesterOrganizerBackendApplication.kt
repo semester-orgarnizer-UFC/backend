14c3dd7ee98de55d814a05461ecba17c4e9abc79
Renê <renejr.arraes286@gmail.com>
2022-07-12 20:09:22 -0300

    infra: init the project

---
+++ src/main/kotlin/com/ufc/semesterorganizerbackend/SemesterOrganizerBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.ufc.semesterorganizerbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SemesterOrganizerBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<SemesterOrganizerBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/ufc/semesterorganizerbackend/SemesterOrganizerBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ufc.semesterorganizerbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SemesterOrganizerBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
